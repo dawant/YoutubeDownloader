90d54cce131ffc7a77b4de88c73a3c91f7be2384
Dawid <152201776+dawidantonik@users.noreply.github.com>
2023-11-27 20:07:07 +0100

    Added more functions:
    - Now you can check the video's information alone,
    - Choice of the resolution when saving (best, worst)
    - Choose the location of the file by pasting folder path.

--- main.py
+++ main.py
@@ -1,9 +1,42 @@
 from pytube import YouTube
-from sys import argv
 
 print("Welcome to the YouTube downloader using pytube!")
-print("Would you like to see info of the video you're about to download?")
-link = str(input(argv[1]))
-youtube = YouTube(link)
+print(" ")
+link = str(input("Paste the link of the video:"))
+print(" ")
+export = str(input("Folder path: "))
+print(" ")
+print("Would you like to see the information about the video, or download it?")
+print(" ")
+print("1. View information")
+print("2. Download video")
+print(" ")
+choice = int(input("Type a number:"))
+print("")
+video = YouTube(link)
+low = video.streams.get_lowest_resolution()
+high = video.streams.get_highest_resolution()
 
-print()
+range = range(3)
+
+if choice == 1:
+        print("Title: ", video.title)
+        print("Views: ", video.views)
+        print("Uploader: ", video.author)
+        print("Length (seconds): ",video.length)
+        print("Age restricted?: ",video.age_restricted)
+if choice == 2:
+     print("Would you like to save the video in the best or the worst resolution?")
+     print("1. Highest resolution")
+     print("2. Lowest resolution")
+answer = int(input())
+if answer == 1:
+     high.download(export)
+     print("The video was saved in the highest quality in " +export +".")
+if answer == 2:
+     low.download(export)
+     print("The video was saved in the lowest quality in " +export +".")
+
+
+
+
