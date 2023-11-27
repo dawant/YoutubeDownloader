ff96b523c2abe5016620a50c0620bf8de8490a6f
Dawid <152201776+dawidantonik@users.noreply.github.com>
2023-11-27 21:19:16 +0100

    The program will now exit after user inputs a number out of range (numbers).

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from pytube import YouTube
+import sys
+
+numbers = range(3)
 
 print("Welcome to the YouTube downloader using pytube!")
 print(" ")
@@ -12,12 +15,13 @@ print("1. View information")
 print("2. Download video")
 print(" ")
 choice = int(input("Type a number:"))
+if choice not in numbers:
+    print("Our of range, try number 1 or 2.")
+    sys.exit()
 print("")
 video = YouTube(link)
 low = video.streams.get_lowest_resolution()
 high = video.streams.get_highest_resolution()
-
-range = range(3)
 
 if choice == 1:
         print("Title: ", video.title)
@@ -30,6 +34,9 @@ if choice == 2:
      print("1. Highest resolution")
      print("2. Lowest resolution")
 answer = int(input())
+if answer not in numbers:
+    print("Our of range, try number 1 or 2.")
+    sys.exit()
 if answer == 1:
      high.download(export)
      print("The video was saved in the highest quality in " +export +".")
